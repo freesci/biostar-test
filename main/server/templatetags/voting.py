ae012944535cde694d23d665cf80191190a426c3
portalmlekozyje <portalmlekozyje@back3>
2013-09-18 13:43:36 +0200

    initial copy of biostar-central, no settings.py

---
+++ main/server/templatetags/voting.py
@@ -0,0 +1,19 @@
+from django import template
+from main.server import const
+
+'''
+register = template.Library()
+
+def votebox(context, post, up, down):
+    "This generates the up/down arrows"
+    return { 'post':post, 
+             'upvoted': post.id in up,
+             'downvoted': post.id in down,
+    }
+
+register.inclusion_tag('widgets/vote.box.html', takes_context=True)(votebox)
+'''
+
+
+
+
