1761da2d8c4dfe965126d1a2750f2a56db5333b2
MegiTeam <admin@megiteam.pl>
2013-09-18 13:33:19 +0200

    Middleware to retrieve correct remote address from X-Forwarded-For

--- wwwportalmlekozyjestart/settings.py
+++ wwwportalmlekozyjestart/settings.py
@@ -99,6 +99,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'wwwportalmlekozyjestart.middleware.XForwardedForMiddleware',
 )
 
 ROOT_URLCONF = 'wwwportalmlekozyjestart.urls'
